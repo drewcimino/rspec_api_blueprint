c04de0c0a5d927a24bd2475b26a57e08716c3f4f
Drew Cimino <dscimino@gmail.com>
2018-01-22 00:50:27 +0300

    broke out documentation writing into a few methods

--- lib/rspec_api_blueprint.rb
+++ lib/rspec_api_blueprint.rb
@@ -9,6 +9,54 @@ def doc_file_path(file_name)
   File.join RspecApiBlueprint.configuration.docs_folder, "#{file_name}.md"
 end
 
+def documentation_for(example, request, response)
+  "## #{example.metadata[:full_description] || action}\n\n".tap do |doc_string|
+    doc_string << request_doc(request)
+    doc_string << response_doc(response)
+  end
+end
+
+def request_doc(request)
+  request_body = request.body.read
+  authorization_header = (request.env && request.env['Authorization']) || request.headers['Authorization']
+
+  if request_body.present? || authorization_header.present?
+    "+ Request #{request.content_type}\n\n".tap do |request_doc_string|
+
+      # Request Headers
+      if authorization_header.present?
+        request_doc_string << "+ Headers\n\n".indent(4)
+        request_doc_string << "Authorization: #{authorization_header}\n\n".indent(12)
+      end
+
+      # Request Body
+      if request_body.present? && request.content_type == 'application/json'
+        request_doc_string << "+ Body\n\n".indent(4) if authorization_header
+        request_doc_string << "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header.present? ? 12 : 8)
+      end
+    end
+  else
+    ''
+  end
+end
+
+def response_doc(response)
+  "+ Response #{response.status} #{response.content_type}\n\n".tap do |response_doc_string|
+
+    # Response Headers
+    if response.headers.any?
+      response_doc_string << "+ Headers\n\n".indent(4)
+      response_doc_string << response.headers.map { |name, value| "#{name}: #{value}\n".indent(12) }.join
+      response_doc_string << "\n"
+    end
+
+    if response.body.present? && response.content_type == 'application/json'
+      response_doc_string << "+ Body\n\n".indent(4)
+      response_doc_string << "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(response.headers.any? ? 12 : 8)
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.before(:suite) do
     $rspec_api_blueprinted_spec_documents ||= {}
@@ -33,44 +81,7 @@ RSpec.configure do |config|
       file_name = path.sub(/^\.\/spec\/(api|integration|request)\//, '').underscore
 
       # Resource & Action
-      spec_doc = "## #{example.metadata[:full_description] || action}\n\n"
-
-      # Request
-      request_body = request.body.read
-      authorization_header = (request.env && request.env['Authorization']) || request.headers['Authorization']
-
-      if request_body.present? || authorization_header.present?
-        spec_doc << "+ Request #{request.content_type}\n\n"
-
-        # Request Headers
-        if authorization_header.present?
-          spec_doc << "+ Headers\n\n".indent(4)
-          spec_doc << "Authorization: #{authorization_header}\n\n".indent(12)
-        end
-
-        # Request Body
-        if request_body.present? && request.content_type == 'application/json'
-          spec_doc << "+ Body\n\n".indent(4) if authorization_header
-          spec_doc << "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header.present? ? 12 : 8)
-        end
-      end
-
-      # Response
-      spec_doc << "+ Response #{response.status} #{response.content_type}\n\n"
-
-      # Response Headers
-      if response.headers.any?
-        spec_doc << "+ Headers\n\n".indent(4)
-        response.headers.each do |name, value|
-          spec_doc << "#{name}: #{value}\n".indent(12)
-        end
-        spec_doc << "\n"
-      end
-
-      if response.body.present? && response.content_type == 'application/json'
-        spec_doc << "+ Body\n\n".indent(4)
-        spec_doc << "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(response.headers.any? ? 12 : 8)
-      end
+      spec_doc = documentation_for(example, request, response)
 
       $rspec_api_blueprinted_spec_documents[file_name] ||= {}
       $rspec_api_blueprinted_spec_documents[file_name][example.metadata[RspecApiBlueprint.configuration.sort_key]] = spec_doc
